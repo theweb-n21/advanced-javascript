ab51853ce6e9bede9f03ceb2e91cde9205465c24
MD.Nazmul islam <82874193+theweb-n21@users.noreply.github.com>
2021-07-24 14:08:15 +0600

    true vs false

---
+++ 23-2-true-false.js
@@ -0,0 +1,17 @@
+// Falsy
+// 0
+// ''
+// undefined
+// null
+// NaN
+// false
+let name;
+console.log(name);
+
+
+if(name){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
