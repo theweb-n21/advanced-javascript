e71a18b7ed6675f88cdd4eb107793e2f52d1bbea
MD.Nazmul islam <82874193+theweb-n21@users.noreply.github.com>
2021-07-24 18:27:44 +0600

    null vs undefind cleared

---
+++ 23-3-null vs undefined.js
@@ -0,0 +1,14 @@
+let pakhi;
+const add = (num1, num2) =>{
+    console.log(num1 + num2);
+}
+const add = (num1, num2) =>{
+    console.log(num1, num2)
+}
+const student = {name: 'sunny', rollNo: 12, class: 10};
+const {id} = student;
+ 
+let fun = undefined;
+
+let ages = [2, 5, 6];
+console.log(ages[11]);
